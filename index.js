885adefe13c47e45109ed8effdfadc9cf28c2cd4
qwe12389 <tessst0001@outlook.com>
2025-05-20 09:19:51 +0600

    Update index.js

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ app.post('/artify', upload.single('image'), (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
